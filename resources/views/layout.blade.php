9f9790279d66e9b2afd89b819e9153a8f3da2374
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2017-01-12 15:24:51 +0100

    Add - Layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8" />
+        
+        <link href="css/main.css" rel="stylesheet" type="text/css" />
+        <link href="css/layout.css" rel="stylesheet" type="text/css" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        
+        
+        <title>Laravel</title>
+    </head>
+    <body>
+    
+    <header>
+
+    </header>
+        
+    <div id="page">
+        
+        <div id="content">
+
+            @yield('content')
+
+        </div><!-- content -->
+    
+    </div><!-- page -->
+    
+    <footer>
+
+    </footer>
+    
+    </body>
+</html>

6e4ae12a6fa02762d1b73a49f68bc7324cb087c6
Unknown <loic.dessaules@gmail.com>
2017-01-14 16:29:20 +0100

    Added form builder to Laravel and updated link
    I added a form builder to Laravel : https://laravelcollective.com/docs/5.0/html
    
    I updated <a> link, now I use route() more efficient.

--- resources/views/sport/index.blade.php
+++ resources/views/sport/index.blade.php
@@ -12,12 +12,12 @@
 				<tr>
 					<td class="name">{{$sport->name}}</td>
 					<td class="action">
-						<a href="{{url('sports/'.$sport->id.'/edit')}}" title="Éditer le sport"><i class="fa fa-pencil"></i></a>
+						<a href="{{route('sports.edit',$sport->id)}}" title="Éditer le sport"><i class="fa fa-pencil"></i></a>
 						<i class="fa fa-trash-o" aria-hidden="true"></i>
 					</td>
 				</tr>
 			@endforeach
 		</table>
-		<a href="{{url('sports/create')}}" title="Créer un sport"><input type="button" value="Nouveau"></a>
+		<a href="{{route('sports.create')}}" title="Créer un sport"><input type="button" value="Nouveau"></a>
 	</div>
 @stop

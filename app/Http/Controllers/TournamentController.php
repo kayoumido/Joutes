84e2ad0a520b2690a7364a72c47eb5d0da7eb415
vagrant <doran.kayoumi@cpnv.ch>
2017-01-17 17:12:11 +0000

    Implemented index and show. index -> return JSON of all tournaments of an event, show -> return JSON of specific tournament of an event

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Tournament;
+use App\Event;
+use App\Sport;
+
 class TournamentController extends Controller
 {
     /**
@@ -11,9 +15,30 @@ class TournamentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($event)
     {
-        //
+        $tournaments = Event::findOrFail($event)->tournaments;
+
+        // for ($i = 0; $i < count($tournaments); ++$i) {
+        foreach ($tournaments as $tournament) {
+
+            $court_names = [];
+            $sport       = '';
+
+            $courts = Tournament::findOrFail($tournament->id)->courts;
+
+            foreach ($courts as $court) {
+                $court_names[] = $court->name;
+                $sport         = Sport::findOrFail($court->fk_sports);
+            }
+
+            // adding found sport and courts to tournaments array.
+            // This is done because, these informations aren't found when getting info from tournament.
+            $tournament['sport']  = $sport->name;
+            $tournament['courts'] = $court_names;
+        }
+
+        return $tournaments;
     }
 
     /**
@@ -43,9 +68,33 @@ class TournamentController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($event, $tournament_id)
     {
-        //
+        $court_names = [];
+        $sport       = '';
+        $team_names  = [];
+
+        $tournament = Event::findOrFail($event)->tournaments->where('id', $tournament_id);
+
+        $courts = Tournament::findOrFail($tournament_id)->courts;
+        $teams  = Tournament::findOrFail($tournament_id)->teams;
+
+        foreach ($courts as $court) {
+            $court_names[] = $court->name;
+            $sport         = Sport::findOrFail($court->fk_sports);
+        }
+
+        foreach ($teams as $team) {
+            $team_names[] = $team->name;
+        }
+
+        // adding found sport and courts to tournaments array.
+        // This is done because, these informations aren't found when getting info from tournament.
+        $tournament['sport']  = $sport->name;
+        $tournament['courts'] = $court_names;
+        $tournament['teams']  = $team_names;
+
+        return $tournament;
     }
 
     /**

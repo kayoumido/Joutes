5ee7b6a31e4df43e5c654c2f73b8a15212c6a0fb
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2017-01-17 13:50:48 +0100

    Delete teams without pop-up

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -17,10 +17,14 @@
 
 		  	@foreach ($teams as $team)
 				<tr>
-			      <td> {{ $team->name }} {{ $team->id }}</td>
+			      <td> <a href="{{ route('teams.show', $team->id) }}" alt="Afficher la team"> {{ $team->name }} </a> </td>
 			      <td> 
 				      <a href="{{ route('teams.edit', $team->id) }}" alt="Modifier la team"> <i class="fa fa-pencil" aria-hidden="true"></i> </a>
-				      <a href="" alt="Supprimer la team"> <i class="fa fa-trash-o" aria-hidden="true"></i> </a>
+				      {{ Form::open(array('url' => route('teams.destroy', $team->id), 'method' => 'delete')) }}
+				      	<button type="submit" class="button-delete">
+				      		<i class="fa fa-trash-o" aria-hidden="true"></i>
+				      	</button>
+				      {{ Form::close() }}
 			      </td>
 			    </tr>
 

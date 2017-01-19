6d191255096b6c854d36313fc967fa841545aa22
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2017-01-19 13:42:55 +0100

    Fix CSS merge

--- resources/views/team/show.blade.php
+++ resources/views/team/show.blade.php
@@ -2,46 +2,51 @@
 
 @section('content')
 
-	<h1> {{ $team->name }}</h1>
-	<h2>Membres de l'équipe</h2>
+	
+	<div id="container">
+		<a href="{{ route('teams.index') }}"><img src="{{ asset("images/return-arrow.png") }}" alt="Retour en arrière" class="return"></a>
 
-	<table>
+		<h1> {{ $team->name }}</h1>
+		<h2>Membres de l'équipe</h2>
 
-		<thead>
-			<tr>
-				<th>Nom du membre</th>
-				<th>Options</th>
-			</tr>
-		</thead>
+		<table>
 
-		<tbody>
-
-		  	@foreach ($team->participants as $participant) 
+			<thead>
 				<tr>
-			      <td> {{ $participant->last_name }}{{ $participant->first_name }} </td>
-			      <td> 
-				      {{ Form::open(array('url' => route('teams.participants.destroy', [$participant->pivot['fk_participants'], $participant->pivot['fk_teams']]), 'method' => 'delete')) }}
-				      	<button type="submit" class="button-delete">
-				      		<i class="fa fa-trash-o" aria-hidden="true"></i>
-				      	</button>
-				      {{ Form::close() }}
-			      </td>
-			    </tr>
+					<th>Nom du membre</th>
+					<th>Options</th>
+				</tr>
+			</thead>
 
+			<tbody>
+
+			  	@foreach ($team->participants as $participant) 
+					<tr>
+				      <td> {{ $participant->last_name }}{{ $participant->first_name }} </td>
+				      <td> 
+					      {{ Form::open(array('url' => route('teams.participants.destroy', [$participant->pivot['fk_participants'], $participant->pivot['fk_teams']]), 'method' => 'delete')) }}
+					      	<button type="submit" class="button-delete">
+					      		<i class="fa fa-trash-o .button-delete" aria-hidden="true"></i>
+					      	</button>
+					      {{ Form::close() }}
+				      </td>
+				    </tr>
+
+				@endforeach
+		    	
+		  	</tbody>
+
+		</table>
+
+		<h2>Ajouter un membre</h2>
+		<select>
+			<option> Séléctionner </option>
+			@foreach ($pepoleNoTeam as $pepole)
+				<option> {{ $pepole->last_name }} {{ $pepole->first_name }} </option>
 			@endforeach
-	    	
-	  	</tbody>
 
-	</table>
+		</select>
 
-	<h2>Ajouter un membre</h2>
-	<select>
-		<option> Séléctionner </option>
-		@foreach ($pepoleNoTeam as $pepole)
-			<option> {{ $pepole->last_name }} {{ $pepole->first_name }} </option>
-		@endforeach
-
-	</select>
-
+	</div>
 	
 @stop

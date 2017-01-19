115275f8a82149f453046444269166bd7b8eb1e6
vagrant <doran.kayoumi@cpnv.ch>
2017-01-19 15:45:49 +0000

    Added function to link teams with tournaments

--- app/Team.php
+++ app/Team.php
@@ -4,7 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Team extends Model
-{
-    //
+class Team extends Model {
+
+    public function tournaments() {
+        return $this->belongsToMany('App\Tournament', 'tournaments_has_teams', 'fk_teams', 'fk_tournaments');
+    }
 }

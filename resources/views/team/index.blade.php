3fb6068b37f859e01afaea94560ab4b0e0446363
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2017-01-19 13:33:30 +0100

    Fix CSS merge

--- resources/views/team/index.blade.php
+++ resources/views/team/index.blade.php
@@ -2,38 +2,43 @@
 
 @section('content')
 
-	<h1>Teams</h1>
-	
-	<input type="search" placeholder="Recherche" class="search">
+	<div id="container">
+		<a href="/"><img src="{{ asset("images/return-arrow.png") }}" alt="Retour en arrière" class="return"></a>
 
-	<table>
+		<h1>Teams</h1>
+		
+		<input type="search" placeholder="Recherche" class="search form-control">
 
-		<thead>
-			<tr>
-				<th>Nom de l'équipe</th>
-				<th>Options</th>
-			</tr>
-		</thead>
+		<table>
 
-		<tbody class="searchIn">
-
-		  	@foreach ($teams as $team)
+			<thead>
 				<tr>
-			      <td class="name"> <a href="{{ route('teams.show', $team->id) }}" alt="Afficher la team"> {{ $team->name }} </a> </td>
-			      <td> 
-				      <a href="{{ route('teams.edit', $team->id) }}" alt="Modifier la team"> <i class="fa fa-pencil" aria-hidden="true"></i> </a>
-				      {{ Form::open(array('url' => route('teams.destroy', $team->id), 'method' => 'delete')) }}
-				      	<button type="submit" class="button-delete">
-				      		<i class="fa fa-trash-o" aria-hidden="true"></i>
-				      	</button>
-				      {{ Form::close() }}
-			      </td>
-			    </tr>
+					<th>Nom de l'équipe</th>
+					<th>Options</th>
+				</tr>
+			</thead>
 
-			@endforeach
-	    	
-	  	</tbody>
+			<tbody class="searchIn">
 
-	</table>
+			  	@foreach ($teams as $team)
+					<tr>
+				      <td class="name"> <a href="{{ route('teams.show', $team->id) }}" alt="Afficher la team"> {{ $team->name }} </a> </td>
+				      <td> 
+					      <a href="{{ route('teams.edit', $team->id) }}" alt="Modifier la team"> <i class="fa fa-pencil" aria-hidden="true"></i> </a>
+					      {{ Form::open(array('url' => route('teams.destroy', $team->id), 'method' => 'delete')) }}
+					      	<button type="submit" class="button-delete">
+					      		<i class="fa fa-trash-o" aria-hidden="true"></i>
+					      	</button>
+					      {{ Form::close() }}
+				      </td>
+				    </tr>
+
+				@endforeach
+		    	
+		  	</tbody>
+
+		</table>
+
+	</div>
 
 @stop

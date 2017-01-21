9b297b4d8c95ba1574878ee3f6d10ce49b3f5fef
Unknown <loic.dessaules@gmail.com>
2017-01-21 15:07:29 +0100

    Updated and finished show method

--- resources/views/tournament/show.blade.php
+++ resources/views/tournament/show.blade.php
@@ -5,17 +5,25 @@
 		<a href="{{ route('tournaments.index') }}"><img src="{{ asset("images/return-arrow.png") }}" alt="Retour en arrière" class="return"></a>
 		<h1>{{ $tournament->name }}</h1>
 
-		<p>Sport : {{ $tournament->courts[0]->sport->name }} </p>
-		<p>Début du tournois : {{ date("d.m.Y", strtotime($tournament->start_date)) }} à {{ date("H:i", strtotime($tournament->start_time)) }}</p>
-		<p>Date de fin : {{ date("d.m.Y", strtotime($tournament->end_date)) }}</p>
-		<p>Terrains :</p>
-		<ul>
-			@foreach ($tournament->courts as $court)
-				<li> {{ $court->name }} </li>
-			@endforeach
-		</ul>
-
+		<p><strong>Sport :</strong> {{ $tournament->courts[0]->sport->name }}</p>
+		<p>
+			<strong>Date de début :</strong> {{ date("d.m.Y", strtotime($tournament->start_date)) }}
+			<br>
+			<strong>Date de fin :</strong> {{ date("d.m.Y", strtotime($tournament->end_date)) }}
+		</p>
+		<p><strong>Heure de début :</strong> {{ date("H:i", strtotime($tournament->start_time)) }} </p>
 		
+		<div class="list-group">
+		  	<div class="list-group-item active" style="background-color:#636b6f; border-color: #636b6f">
+		    	<h4 class="list-group-item-heading">Terrain(s)</h4>
+		  	</div>
+		  	@foreach ($tournament->courts as $court)
+		  		<div class="list-group-item">
+					{{ $court->name }}
+				</div>
+			@endforeach
+		</div>
+
 
 	</div>
 @stop

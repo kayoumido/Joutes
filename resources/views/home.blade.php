75c062ea272d110487bf7bbb5848376a4d83eb7b
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2017-01-24 11:08:17 +0100

    Remove unused block

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,45 +4,40 @@
 	
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+			<div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
 				<a href="{{ route('sports.index') }}" title="Voir tous les sports">
 					<div class="tile">
 						Sports
 					</div>
 				</a>
 			</div>
-			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+			<div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
 				<a href="{{ route('courts.index') }}" title="Voir tous les terrains">
 					<div class="tile">
 						Terrains
 					</div>
 				</a>
 			</div>
-			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+			<div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
 				<a href="{{ route('tournaments.index') }}" title="Voir tous les tournois">
 					<div class="tile">
 						Tournois
 					</div>
 				</a>
 			</div>
-			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+			<div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
 				<a href="{{ route('teams.index') }}" title="Voir toutes les équipes">
 					<div class="tile">
 						Équipes
 					</div>
 				</a>
 			</div>
-			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
+			<div class="col-lg-4 col-md-3 col-sm-4 col-xs-6">
 				<a href="{{ route('participants.index') }}" title="Voir toutes les participants">
 					<div class="tile">
 						Participants
 					</div>
 				</a>
-			</div>
-			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">
-				<div class="tile">
-					Some text
-				</div>
 			</div>
 		</div>
 	</div>

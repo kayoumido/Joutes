69255014e9eeaaa0b60ca8e4cde307dd01e83239
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-02-03 14:44:50 +0100

    Corrected menu. Now we can navigate trough app

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,7 +15,7 @@
         <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" />
 
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        
+
         <title>Laravel</title>
     </head>
     <body>
@@ -28,16 +28,18 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="/">
                         <img src="{{ asset('images/logo.png') }}" alt="">
                     </a>
                 </div>
                 <div id="navbar-collapse" class="navbar-collapse collapse" aria-expanded="false">
                     <ul class="nav navbar-nav">
-                        <li><a href="index.html">Accueil</a></li>
-                        <li><a href="#Formations">Formations</a></li>
-                        <li><a href="#Admissions">Admissions</a></li>
-                        <li><a href="#Entreprise">Entreprise</a></li>
+                        <li><a href="/">Accueil</a></li>
+                        <li><a href="{{ route('sports.index') }}">Sports</a></li>
+                        <li><a href="{{ route('courts.index') }}">Terrains</a></li>
+                        <li><a href="{{ route('tournaments.index') }}">Tournois</a></li>
+                        <li><a href="{{ route('teams.index') }}">Equipes</a></li>
+                        <li><a href="{{ route('participants.index') }}">Participants</a></li>
                     </ul>
                 </div>
             </nav>
@@ -56,7 +58,7 @@
         <footer>
 
         </footer>
-        
+
         <script src="{{ asset('js/app.js') }}"></script>
         <script src="{{ asset('js/sweetalert.min.js') }}"></script>
 

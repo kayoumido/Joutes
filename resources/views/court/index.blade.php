89678693f160a4ac439fe2d7f13de8e74717a3fd
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-02-10 12:51:39 +0100

    Corrected merge problem on court index view

--- resources/views/court/index.blade.php
+++ resources/views/court/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 	<div class="container">
-		<a href="/"><img src="{{ asset("images/return-arrow.png") }}" alt="Retour en arrière" class="return"></a>
+		<a href="/"><i class="fa fa-4x fa-arrow-circle-left return" aria-hidden="true"></i></a>
 		<h1>Terrains</h1>
 		<table class="table">
 			<thead>
@@ -20,10 +20,10 @@
 						<th scope="row" class="name" style="width:40%">{{$court->name}}</th>
 						<td class="description" style="width:40%">{{$court->sport->name}}</td>
 						<td class="action">
-							<a href="{{route('courts.edit',$court->id)}}" title="Éditer le terrain" class="edit"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+							<a href="{{route('courts.edit',$court->id)}}" title="Éditer le terrain" class="edit"><i class="fa fa-pencil fa-lg action" aria-hidden="true"></i></a>
 							{{ Form::open(array('url' => route('courts.destroy', $court->id), 'method' => 'delete')) }}
 								<button type="button" class="button-delete" data-name="{{ $court->name }}" data-type="court">
-				                    <i class="fa fa-trash-o" aria-hidden="true"></i>
+				                    <i class="fa fa-trash-o fa-lg action" aria-hidden="true"></i>
 				                </button>
 							{{ Form::close() }}
 						</td>

8b85c891e5c5bc32447148ed9c0f49ba6d758ef2
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-02-17 11:52:03 +0100

    Created array to contain file names

--- app/Http/Controllers/EventImportController.php
+++ app/Http/Controllers/EventImportController.php
@@ -28,6 +28,34 @@ class EventImportController extends Controller {
      * @author Doran Kayoumi
      */
     public function store(Request $request) {
+        // temporary path building
+        $path = storage_path() . '/xml';
 
+        $filenames = array(
+            'participants' => array(
+                'folder' => 'FichiersXML',
+                'names'  => array(
+                    'info_enseignants.xml',
+                    'info_etudiants.xml',
+                    'meca_enseignants.xml',
+                    'meca_etudiants.xml',
+                    'media_enseignants.xml',
+                    'media_etudiants.xml'
+                )
+            ),
+            'activities' => array(
+                'folder' => 'FichiersXML',
+                'names'  => array(
+                    'Activites.xml'
+                )
+            ),
+            'teams' => array(
+                'folder' => 'Activites',
+                'names'  => array(
+                    'Equipes.xml',
+                    'Participants.xml'
+                )
+            ),
+        );
     }
 }

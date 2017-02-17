52919b0fe1282654bcf164ab7108fabd0dc3b099
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-02-17 11:32:31 +0100

    Added attribute to class so i can use Participant::create()

--- app/Participant.php
+++ app/Participant.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+    // I added this because when I try to save() Sport value an updated_At "xy" error appears
+    // And with this that work
+    public $timestamps = false;
+    protected $fillable = array('id', 'first_name', 'last_name'); // -> We have to define all data we use on our courts table (For use : ->all())
+
     public function teams()
     {
         return $this->belongsToMany('App\Team', 'participants_has_teams', 'fk_participants' ,'fk_teams');

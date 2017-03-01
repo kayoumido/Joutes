29c4f4d30b5d0ff716c00bf84cb0d409415e3f35
CPNV-SC\Antoine.DESSAUGES <antoine.dessauges@cpnv.ch>
2017-03-01 15:40:17 +0100

    Add poolMode and stage in pools

--- database/migrations/2017_02_15_140645_create_pools_table.php
+++ database/migrations/2017_02_15_140645_create_pools_table.php
@@ -17,7 +17,9 @@ class CreatePoolsTable extends Migration
             $table->increments('id'); 
             $table->time('start_time'); 
             $table->time('end_time')->nullable();
-            $table->string('poolName', 100); 
+            $table->string('poolName', 100);
+            $table->string('poolMode', 100); 
+            $table->integer('stage'); 
             $table->integer('tournaments_id')->unsigned(); 
  
             $table->foreign('tournaments_id')->references('id')->on('tournaments'); 

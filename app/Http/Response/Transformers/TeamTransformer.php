69298afa978fddc1b084640e0103d31ea4a6187d
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-15 14:26:46 +0100

    Created team transformer

---
+++ app/Http/Response/Transformers/TeamTransformer.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Response\Transformers;
+
+use App\Tournament;
+use App\Team;
+use League\Fractal\TransformerAbstract;
+
+class TeamTransformer extends TransformerAbstract
+{
+    public function transform(Team $team) {
+
+        return [
+            'id'     => (int) $team->id,
+            'name'   => (string) $team->name,
+            'sport'  => $team->sport->name,
+        ];
+    }
+}

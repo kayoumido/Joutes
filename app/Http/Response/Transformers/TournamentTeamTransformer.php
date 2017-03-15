a19f81b0fd735282a95837a1f5ce41b58b9184d5
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-15 16:08:33 +0100

    Created TournamentTeamTransformer
    This will change Tournamen teams structure to only contain id and name

---
+++ app/Http/Response/Transformers/TournamentTeamTransformer.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Response\Transformers;
+
+
+use App\Team;
+use League\Fractal\TransformerAbstract;
+
+class TournamentTeamTransformer extends TransformerAbstract
+{
+    public function transform(Team $team) {
+
+        return [
+            'id'   => $team->id,
+            'name' => $team->name,
+        ];
+    }
+}

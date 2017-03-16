f68173b9f0c622b39156b8db07f9d589adc1bf7d
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-16 11:45:12 +0100

    Implement TeamTournamentTransformer to return singel team tournament
    Created TeamTournamentTransformer so can change structure of returned tournament

--- app/Http/Response/Transformers/SingleTeamTransformer.php
+++ app/Http/Response/Transformers/SingleTeamTransformer.php
@@ -8,21 +8,26 @@ use League\Fractal\TransformerAbstract;
 class SingleTeamTransformer extends TransformerAbstract
 {
     public $defaultIncludes = [
-        "members"
+        "members",
+        'tournament',
     ];
 
     public function transform(Team $team) {
 
         return [
-            'id'      => (int) $team->id,
-            'name'    => (string) $team->name,
-            'status'  => (string) '',
-            'sport'   => $team->sport->name,
-            'matches' => [],
+            'id'         => (int) $team->id,
+            'name'       => (string) $team->name,
+            'status'     => (string) '',
+            'sport'      => $team->sport->name,
+            'matches'    => [],
         ];
     }
 
     public function includeMembers(Team $team) {
         return $this->collection($team->participants, new TeamMemberTransformer);
+    }
+
+    public function includeTournament(Team $team) {
+        return $this->item($team->tournament, new TeamTournamentTransformer);
     }
 }

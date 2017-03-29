b549b925f04f240ca9e43e6548ebc7a5a2e996bc
Loïc Dessaules <loic.dessaules@cpnv.ch>
2017-03-29 15:01:08 +0200

    Changed create tournament route
    the route for create a tournament is now : /events/n°/tournaments/create because his linked to an event. So I created the good controler etc...

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,8 @@ Route::resource('admin', 'SessionController', ['only' => ['index', 'store', 'des
 // It will add the "admin" prefix before each "critical" URLs
 Route::group(['middleware'=>'checkIsAdmin', 'prefix'=>'admin', 'namespace' => 'Admin'],function(){
 	Route::resource('events', 'EventController', ['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
-	Route::resource('tournaments', 'TournamentController', ['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
+	Route::resource('tournaments', 'TournamentController', ['only' => ['edit', 'update', 'destroy']]);
+	Route::resource('events.tournaments', 'EventTournamentController', [ 'only' => ['create', 'store']]);
 	Route::resource('sports', 'SportController');
 	Route::resource('courts', 'CourtController');
 	Route::resource('teams', 'TeamController');

311f70c4373eaa3332b11e478342283c14cf00ae
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-29 15:53:25 +0200

    Eddited route
    It wasn't pointing on correct controller

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ $api->version('v1', function ($api) {
             'index', 'show'
         ]]);
 
-        $api->resource('schedule', 'App\Http\Controllers\API\MatchScheduleController', [ 'only' => [
+        $api->resource('schedule', 'App\Http\Controllers\API\ScheduleController', [ 'only' => [
             'index'
         ]]);
     });

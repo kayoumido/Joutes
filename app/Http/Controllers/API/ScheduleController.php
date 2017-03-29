9dfcb1a027a27b48aef8512fb2a7474448468e75
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-29 15:53:48 +0200

    Added use so if could extend Controller class

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class ScheduleController extends Controller
-{
-    //
+class ScheduleController extends Controller {
+
+    public function index(Request $request) {
+        return 'hello';
+    }
 }

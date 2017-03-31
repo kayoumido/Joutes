fc1309371d257649e8cdd632614b28c3545bf3f3
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-31 14:38:35 +0200

    Placed usage of Dingo helper in Controllers parent class

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -3,14 +3,11 @@
 namespace App\Http\Controllers\API;
 
 use App\Game;
-use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Response\Transformers\ScheduleTransformer;
 
 class ScheduleController extends Controller {
-
-    use Helpers;
 
     public function index(Request $request) {
         $games = Game::all();

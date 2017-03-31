fc9c7f99b70cf88d70df48ea4fd574eb707ed670
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-31 10:48:33 +0200

    Created eloquent relations for Game model

--- app/Game.php
+++ app/Game.php
@@ -4,7 +4,29 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Game extends Model
-{
-    //
+class Game extends Model {
+
+    public function contender1() {
+        return $this->belongsTo('App\Contender', 'contender1_id');
+    }
+
+    public function contender2() {
+        return $this->belongsTo('App\Contender', 'contender2_id');
+    }
+
+    public function court() {
+        return $this->belongsTo('App\Court');
+    }
+
+    public function sport() {
+        return $this->court->sport;
+    }
+
+    public function team1() {
+        return $this->contender1->team;
+    }
+
+    public function team2() {
+        return $this->contender1->team;
+    }
 }

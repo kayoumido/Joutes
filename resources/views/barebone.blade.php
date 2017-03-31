cc2cd83631a92190cab3b377514b9b4e7342d6c4
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-31 14:16:04 +0200

    Created a layout without header and footer

---
+++ resources/views/barebone.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8" />
+
+        <title>Joutes - Schedule</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+
+
+            <div id="page">
+
+                <div id="content">
+
+                    @yield('content')
+
+                </div><!-- content -->
+
+            </div><!-- page -->
+
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>

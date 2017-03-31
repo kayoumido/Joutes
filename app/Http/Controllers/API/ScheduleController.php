9e55b9925afdd8a39e8c202c0427a6c0b73996ce
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-31 11:11:45 +0200

    Implemented index method

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Game;
+use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Response\Transformers\ScheduleTransformer;
 
 class ScheduleController extends Controller {
 
+    use Helpers;
+
     public function index(Request $request) {
-        return 'hello';
+        $games = Game::all();
+
+        return $this->response->collection($games, new ScheduleTransformer, ['key' => 'games']);
     }
 }

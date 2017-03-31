8e4341f1375d657a975813c381a930e23d2f2042
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-03-31 15:01:13 +0200

    Started displaying games

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -1,5 +1,23 @@
 @extends('barebone')
 
 @section('content')
-    <h1>Hello</h1>
+    <div class="container-fluid">
+        <h1>Upcomming matches</h1>
+
+        {{-- {{ var_dump($schedule) }} --}}
+
+        @foreach ($schedule as $game)
+            <p>This is user {{ $game->id }}</p>
+            <p>This is user {{ $game->date }}</p>
+            <p>This is user {{ $game->start_time }}</p>
+            <p>This is user {{ $game->court->name }}</p>
+            <p>This is user {{ $game->court->sport->name }}</p>
+
+            <br />
+            <br />
+            <br />
+            <br />
+        @endforeach
+
+    </div>
 @stop

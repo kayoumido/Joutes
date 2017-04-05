0d7c358a92ef6e51756d6c3f4bb38c49fa090d11
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-05 14:03:03 +0200

    Include schdule css into barebone template

--- resources/views/barebone.blade.php
+++ resources/views/barebone.blade.php
@@ -7,6 +7,7 @@
         <title>Joutes - Schedule</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/schedule.css') }}" rel="stylesheet" type="text/css" />
 
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 

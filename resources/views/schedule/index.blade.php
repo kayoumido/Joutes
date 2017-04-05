fdaf310ad5375d09e3ddb44b501ac65c4c2279a4
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-05 14:11:33 +0200

    Removed bootstrap col class

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -7,14 +7,12 @@
         {{-- {{ var_dump($schedule) }} --}}
 
         @foreach ($schedule as $game)
-            <div class="col-md-4">
-                <div class="match" data-id="{{ $game->id }}">
-                    <div class="sport">{{ $game->court->sport->name }}</div>
-                    <div class="contenders">
-                        Team A VS Team B
-                    </div>
-                    <div class="venue">{{ $game->court->name }} at {{ $game->start_time }}</div>
+            <div class="match" data-id="{{ $game->id }}">
+                <div class="sport">{{ $game->court->sport->name }}</div>
+                <div class="contenders">
+                    Team A VS Team B
                 </div>
+                <div class="venue">{{ $game->court->name }} at {{ $game->start_time }}</div>
             </div>
         @endforeach
 

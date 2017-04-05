d07468fcce9411b7674dd6abc777dbbc7f1655f9
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-05 15:11:18 +0200

    Removed unecessary class

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -2,32 +2,33 @@
 
 @section('content')
     <div class="container-fluid">
-        <h1>Upcomming matches</h1>
+        <div class="schedule">
+            <h1>Upcomming matches</h1>
 
-        {{-- {{ var_dump($schedule) }} --}}
+            {{-- {{ var_dump($schedule) }} --}}
 
-        @foreach ($schedule as $game)
-            {{-- <div class="match" data-id="{{ $game->id }}">
-                <div class="sport">{{ $game->court->sport->name }}</div>
-                <div class="contenders">
-                    Team A VS Team B
-                </div>
-                <div class="venue">{{ $game->court->name }} at {{ $game->start_time }}</div>
-            </div> --}}
-            <div class="row schedule-row">
-                <div class="match col-md-12" data-id="{{ $game->id }}">
-                    <div class="info time col-md-3">{{ $game->start_time }}</div>
-                    <div class="info venue col-md-1">A</div>
-                    <div class="contenders col-md-6">
-                        <div class="team-name">Team A</div>
-                        <div class="team-name">Team B</div>
+            @foreach ($schedule as $game)
+                {{-- <div class="match" data-id="{{ $game->id }}">
+                    <div class="sport">{{ $game->court->sport->name }}</div>
+                    <div class="contenders">
+                        Team A VS Team B
                     </div>
-                    <div class="info sport col-md-2 text-right">
-                        <i class="fa fa-apple" aria-hidden="true"></i>
+                    <div class="venue">{{ $game->court->name }} at {{ $game->start_time }}</div>
+                </div> --}}
+                <div class="row">
+                    <div class="match col-md-12" data-id="{{ $game->id }}">
+                        <div class="info time col-md-3">{{ $game->start_time }}</div>
+                        <div class="info venue col-md-1">A</div>
+                        <div class="contenders col-md-6">
+                            <div class="team-name">Team A</div>
+                            <div class="team-name">Team B</div>
+                        </div>
+                        <div class="info sport col-md-2 text-right">
+                            <i class="fa fa-apple" aria-hidden="true"></i>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
-
+            @endforeach
+        </div>
     </div>
 @stop

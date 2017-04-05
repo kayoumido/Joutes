def5f13b61a7375607d521a12ace2ceb31660a46
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-05 15:22:43 +0200

    Removed commented code and page title
    They wern't needed anymore

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -3,18 +3,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="schedule">
-            <h1>Upcomming matches</h1>
-
-            {{-- {{ var_dump($schedule) }} --}}
-
             @foreach ($schedule as $game)
-                {{-- <div class="match" data-id="{{ $game->id }}">
-                    <div class="sport">{{ $game->court->sport->name }}</div>
-                    <div class="contenders">
-                        Team A VS Team B
-                    </div>
-                    <div class="venue">{{ $game->court->name }} at {{ $game->start_time }}</div>
-                </div> --}}
                 <div class="row">
                     <div class="match col-md-12" data-id="{{ $game->id }}">
                         <div class="info time col-md-3">{{ $game->start_time }}</div>

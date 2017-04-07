99da045b377f29e88ef334057a5270ad4cc27b40
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-07 10:56:44 +0200

    Created method to clean array of games
    Removes games where contenders dont have any teams set

--- app/Game.php
+++ app/Game.php
@@ -29,4 +29,46 @@ class Game extends Model {
     public function team2() {
         return $this->contender2->team;
     }
+
+    /**
+     * Clears an array of game objects of contenders with no teams
+     * @param  array $games - array of game objects to clean
+     * @return array - cleaned array
+     *
+     * @author Doran Kayoumi
+     */
+    private function cleanEmptyContender($games)
+    {
+        // loop through games to find ones where contenders don't have teams
+        foreach ($games as $key => $game) {
+
+            // check if contenders have a team
+            if ($game->contender1->team == null || $game->contender2->team == null) {
+                // if not remove it from array
+                unset($games[$key]);
+            }
+        }
+
+        return $games;
+    }
+
+    /**
+     * Get games for schedule
+     * @param  integer  $limit - number of games wanted
+     * @param  time     $after - Time after which games are wanted
+     * @return array - array of games
+     *
+     * @author Doran Kayoumi
+     */
+    public static function schedule($limit, $after = 0) {
+        $games = self::where('start_time', '>=', $after)
+            ->orderBy('date', 'ASC')
+            ->orderBy('start_time', 'ASC')
+            ->get()
+            ->take($limit);
+
+        $games = (new self)->cleanEmptyContender($games);
+
+        return $games;
+    }
 }

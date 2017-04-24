4b72b3e4a1f6f6f7c400d404493dc674d2444300
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-24 14:46:25 +0200

    ScheduleController now only returns view

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 class ScheduleController extends Controller {
 
     public function index() {
-
-        $schedule  = json_decode($this->api->get('schedule'));
-
-        return view('schedule.index', array(
-            'schedule' => $schedule
-        ));
+        return view('schedule.index');
     }
 }

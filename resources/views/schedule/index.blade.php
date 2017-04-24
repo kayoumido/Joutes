4af5db874930f579c51f02fb747f3fbdff622e78
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-04-24 14:45:36 +0200

    Removed blade code that generated html
    It's beeing replaced with javascript

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -3,21 +3,6 @@
 @section('content')
     <div class="container-fluid">
         <div class="schedule">
-            @foreach ($schedule as $game)
-                <div class="row">
-                    <div class="match col-md-12" data-id="{{ $game->id }}">
-                        <div class="info time col-md-3">{{ $game->start_time }}</div>
-                        <div class="info venue col-md-1">A</div>
-                        <div class="contenders col-md-6">
-                            <div class="team-name">Team A</div>
-                            <div class="team-name">Team B</div>
-                        </div>
-                        <div class="info sport col-md-2 text-right">
-                            <i class="fa fa-apple" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
         </div>
     </div>
 @stop

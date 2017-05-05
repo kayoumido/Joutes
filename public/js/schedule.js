0114aadb5209af349b1e3556510d51572f97754e
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-05-05 15:09:29 +0200

    Renamend init and refresh schedule methods

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
-    Schedule.initSchedule(Math.floor($(document).height() / 173));
+    // get number of matches that can be displayed
+    var page_limit = Math.floor($(document).height() / 173);
+
+    Schedule.init(page_limit);
 
     setInterval(function() {
 
         //$('.schedule').empty();
-        Schedule.updateSchedule($('.schedule'));
+        Schedule.refresh($('.schedule'), page_limit);
 
     }, 10 * 1000); // 60 * 1000ms
 });

01ce2e7e81a0a4172672898df10d0a048b8955e8
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-05-08 15:47:25 +0200

    Returned games for schedule are now specific to a tournament

--- app/Http/Controllers/API/ScheduleController.php
+++ app/Http/Controllers/API/ScheduleController.php
@@ -3,40 +3,43 @@
 namespace App\Http\Controllers\API;
 
 use App\Game;
+use App\Pool;
+use App\Tournament;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Response\Transformers\ScheduleTransformer;
 use Carbon\Carbon;
+use Illuminate\Support\Collection;
 
 class ScheduleController extends Controller {
 
-    public function index(Request $request) {
+    public function index(Request $request, $tournament_id) {
 
         $games;
         $dt = Carbon::now("Europe/Berlin");
 
         // check if a next match time was given
         if (empty($request->next)) {
-            $games = $this->getFirst($request->limit, $dt->toTimeString());
+            $games = $this->getFirst($request->limit, $tournament_id, $dt->toTimeString());
         }
         else {
-            $games = $this->refresh($request->limit, $dt->toTimeString(), $request->next, $request->last);
+            $games = $this->refresh($request->limit, $tournament_id, $dt->toTimeString(), $request->next, $request->last);
         }
 
         return $games;
     }
 
-    private function getFirst($limit, $current_time) {
-        $games = Game::schedule($limit, $current_time);
+    private function getFirst($limit, $tournament_id, $current_time) {
+        $games = Tournament::find($tournament_id)->GetActiveGames($limit, $current_time);
 
         return $this->response->collection($games, new ScheduleTransformer);
     }
 
-    private function refresh($limit, $current_time, $next, $last) {
+    private function refresh($limit, $tournament_id, $current_time, $next, $last) {
         // check if an update is needed
         if ($next > $current_time) return json_encode(new \stdClass);
 
-        $games = Game::schedule($limit, $last);
+        $games = Tournament::find($tournament_id)->GetActiveGames($limit, $last);
 
         return $this->response->collection($games, new ScheduleTransformer);
     }

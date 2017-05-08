f25df4c08728a05a1b79242d7a4db271c73c200e
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-05-08 13:45:45 +0200

    tournament id is returned in view

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -6,7 +6,10 @@ use Illuminate\Http\Request;
 
 class ScheduleController extends Controller {
 
-    public function index() {
-        return view('schedule.index');
+    public function index($tournament) {
+
+        return view('schedule.index', array(
+            'tournament_id' => $tournament
+        ));
     }
 }

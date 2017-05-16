81cfca4007d18c5a2b020b888a746cf214bcb578
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-05-16 16:00:09 +0200

    Added function comments and now id of matches is sent instead of game time

--- public/js/class/schedule.js
+++ public/js/class/schedule.js
@@ -1,6 +1,14 @@
 class Schedule {
     constructor() {}
 
+    /**
+     * Initialise a schedule
+     * @param  {int} limit         max number of wanted matches
+     * @param  {int} tournament_id id of the tournament from which matches will be retrieved
+     * @return {void}
+     *
+     * @author Doran Kayoumi
+     */
     static init(limit, tournament_id) {
         var self = new Schedule();
 
@@ -11,23 +19,30 @@ class Schedule {
         });
     }
 
+    /**
+     * Refresh schedule
+     * @param  {dom} container     dom element that contains schedule
+     * @param  {int} limit         max number of wanted matches
+     * @param  {int} tournament_id id of the tournament from which matches will be retrieved
+     * @return {void}
+     *
+     * @author Doran Kayoumi
+     */
     static refresh(container, limit, tournament_id) {
         var self = new Schedule();
 
-        container.children('.row').each(function() {
+        container.children('.schedule-row').each(function() {
 
-            let gametime = $(this).children('.match').children('.time').text();
-            let lastgametime = container.children('.row').last().children('.match').children('.time').text();
+            var currentrow = $(this);
+            let gameid     = $(this).children('.match').data('id');
+            let lastgameid = container.children('.row').last().children('.match').data('id');
 
-            if (!gametime) return true;
-
-
-            var result = self.matches(limit, tournament_id, gametime, lastgametime);
+            var result = self.matches(limit, tournament_id, gameid, lastgameid);
 
             result.done(function(data) {
 
                 if (!$.isEmptyObject(data)) {
-                    self.htmldisplay(data, container)
+                    self.htmldisplay(data, container, currentrow);
                 }
             });
         });
@@ -35,9 +50,9 @@ class Schedule {
 
     /**
      * Get matches from server
-     * @param  {int}  limit number of matches wanted
-     * @param  {time} next  time at which the next match will take place
-     * @param  {time} last  time ar which the last match, client has, will take place
+     * @param  {int} limit number of matches wanted
+     * @param  {int} next  id of the next match that will be played
+     * @param  {int} last  id of the last match in schedule
      * @return {promise}
      *
      * @author Doran Kayoumi
@@ -53,30 +68,36 @@ class Schedule {
                 next  : next,
                 last  : last
             },
-            error : function(xhr, options, error) {
-                console.log(xhr);
-                console.log(options);
-                console.log(error);
+            error : function(xhr) {
+                console.error(`readyState:      ${xhr.readyState}`);
+                console.error(`status:          ${xhr.status}`);
+                console.error(`responseText:    ${xhr.responseText}`);
             }
         });
     }
 
     /**
      * Generates and displays html for schedule
-     * @param  {array} matches array of games returned from api
-     * @param  {class} container container in which the html will be displayed
+     * @param  {array}  matches    array of games returned from api
+     * @param  {class}  container  container in which the html will be displayed
+     * @param  {jquery} row        {optional} row containing outdated match
      * @return {void}
      *
      * @author Doran Kayoumi
      */
-    htmldisplay(matches, container) {
+    htmldisplay(matches, container, currentrow = null) {
+
+        // check if a row was given
+        if (currentrow) {
+            currentrow.remove();
+        }
 
         // loop through schedule
         for (let match of matches) {
 
-            // generate html and added immediately to container
+            // generate html and add it immediately to container
             container.append(`
-                <div class="row">
+                <div class="row schedule-row">
                     <div class="match col-md-12" data-id="${match.id}">
                         <div class="info time col-md-2">${match.time}</div>
                         <div class="info venue col-md-2 text-center">${match.court}</div>

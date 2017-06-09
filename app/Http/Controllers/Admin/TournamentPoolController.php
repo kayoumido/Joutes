949dfbdfdd1c45f55122a9761f8ef88009380341
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2017-06-09 14:38:57 +0200

    Created controller for tournament pools

---
+++ app/Http/Controllers/Admin/TournamentPoolController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Pool;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TournamentPoolController extends Controller
+{
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     *
+     * @author Doran Kayoumi
+     */
+    public function update(Request $request, $id) {
+        echo "coucou";
+    }
+}
